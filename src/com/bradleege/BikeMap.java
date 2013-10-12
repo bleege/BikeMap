be15e8e0fe0b94094bc5a7300c21f1a654af4bc4
Brad Leege <bleege@gmail.com>
2013-10-11 22:28:23 -0500

    Adding OSMDroid JavaDoc and configuring some basic map controls

--- src/com/bradleege/BikeMap.java
+++ src/com/bradleege/BikeMap.java
@@ -2,6 +2,9 @@ package com.bradleege;
 
 import android.app.Activity;
 import android.os.Bundle;
+import org.osmdroid.api.IGeoPoint;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
 
 public class BikeMap extends Activity
 {
@@ -13,5 +16,13 @@ public class BikeMap extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        // Interact With the MapView
+        MapView mapView = (MapView)findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+        mapView.setClickable(true);
+        mapView.setMultiTouchControls(true);
+        mapView.getController().setZoom(5);
+        mapView.getController().setCenter(new GeoPoint(44.123, -89.456));
     }
 }
